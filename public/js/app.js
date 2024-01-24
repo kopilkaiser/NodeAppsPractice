bb8ccd0f3e6b4fd02723af502858f7405042ee31
Kopil Kaiser <kopilkaiser24@gmail.com>
2024-01-24 01:45:50 +0000

    minor css stylings applied in one-ui page

--- public/js/app.js
+++ public/js/app.js
@@ -9,38 +9,40 @@ let prices = []; //done
 let quantities = [];
 let totalPrice = 0;
 
-btnSubmit.addEventListener("click", function () {
-  fruitPrices.forEach((price) => {
-    let stringVal = price.textContent;
-    let formattedPrice = stringVal.replace("£", "");
-    let numberPrice = Number(formattedPrice) || 0;
-    console.log(numberPrice);
-    prices.push(numberPrice);
+if (btnSubmit) {
+  btnSubmit.addEventListener("click", function () {
+    fruitPrices.forEach((price) => {
+      let stringVal = price.textContent;
+      let formattedPrice = stringVal.replace("£", "");
+      let numberPrice = Number(formattedPrice) || 0;
+      console.log(numberPrice);
+      prices.push(numberPrice);
+    });
+    console.log(prices);
+    console.log(`prices: ` + prices);
+    for (let qtyInput of fruitQuantities) {
+      let qtyStrVal = qtyInput.value;
+      let qtyNumVal = Number(qtyStrVal) || 0;
+      quantities.push(qtyNumVal);
+    }
+    console.log(quantities);
+    console.log(`quantities: ` + quantities);
+
+    for (let i = 0; i < prices.length; i++) {
+      let calcValue = prices[i] * quantities[i];
+      console.log(`calcValue[${i}]: ${calcValue}`);
+      totalPrice += calcValue;
+    }
+
+    var url =
+      "http://localhost:2222/fruitcart?totalPrice=" + totalPrice.toFixed(2);
+    this.blur();
+    setTimeout(() => {
+      window.location.href = url;
+    }, 500);
   });
-  console.log(prices);
-  console.log(`prices: ` + prices);
-  for (let qtyInput of fruitQuantities) {
-    let qtyStrVal = qtyInput.value;
-    let qtyNumVal = Number(qtyStrVal) || 0;
-    quantities.push(qtyNumVal);
-  }
-  console.log(quantities);
-  console.log(`quantities: ` + quantities);
 
-  for (let i = 0; i < prices.length; i++) {
-    let calcValue = prices[i] * quantities[i];
-    console.log(`calcValue[${i}]: ${calcValue}`);
-    totalPrice += calcValue
-  }
-
-  var url = "http://localhost:2222/fruitcart?totalPrice=" + totalPrice.toFixed(2);
-  this.blur();
-  setTimeout(() => {
-    window.location.href = url;
-  }, 500);
-});
-
-btnSubmit.addEventListener('mouseout', function(){
-  this.blur();
-})
-
+  btnSubmit.addEventListener("mouseout", function () {
+    this.blur();
+  });
+}
